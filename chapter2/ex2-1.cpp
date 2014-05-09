bf6035ed0e0cf9060392081ed9ec51f0002312c2
Chanathip Srithanrat <omongo@gmail.com>
2014-05-09 21:26:54 +0700

    Add ex2-1.cpp, ex2-2.cpp

---
+++ chapter2/ex2-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+  using std::cout;
+  using std::cin;
+  for (int line = 0; line < 4; ++line) {
+    for (int space = line; space > 0; --space)
+      cout << ' ';
+    for (int hash = 8 - line * 2; hash >= 1; --hash)
+      cout << '#';
+    cout << '\n';
+  }
+  return 0;
+}
---
+++ chapter2/ex2-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+int main() {
+  using std::cout;
+  using std::cin;
+  for (int line = -4; line <= 4; ++line) {
+    if (!line)
+      continue;
+    for (int space = abs(line); space > 1; --space)
+      cout << ' ';
+    for (int hash = 10 - abs(line) * 2; hash > 0; --hash)
+      cout << '#';
+    cout << '\n';
+  }
+  return 0;
+}
